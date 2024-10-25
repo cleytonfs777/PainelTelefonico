536720a5c2fe831d4e158baced912484e70f67a4
cleytonfs777 <cleytonfs777@gmail.com>
2024-10-25 08:36:55 -0300

    ultimos ajustes

--- index.py
+++ index.py
@@ -36,14 +36,11 @@ df_geral = pd.read_csv(os.path.join('assets', 'df_geral.csv'), sep=',')
 
 # Converter 'Tempo' para datetime
 df_geral['Tempo'] = pd.to_datetime(df_geral['Tempo'])
-df_geral['dia'] = df_geral['Tempo'].dt.day  # Extrair o dia para o gráfico
-# Extrair o mês a partir de 'Tempo'
+df_geral['dia'] = df_geral['Tempo'].dt.day
 df_geral['Mês'] = df_geral['Tempo'].dt.month
-df_geral['Ano'] = df_geral['Tempo'].dt.year  # Extrair o ano
+df_geral['Ano'] = df_geral['Tempo'].dt.year
 
 # Função para definir a faixa horária
-
-
 def definir_faixa_horaria(hora):
     if 0 <= hora < 2:
         return '00:00-02:00'
@@ -70,10 +67,7 @@ def definir_faixa_horaria(hora):
     else:
         return '22:00-00:00'
 
-
-# Adicionar a nova coluna 'faixa_horaria' ao DataFrame
-df_geral['faixa_horaria'] = df_geral['Tempo'].dt.hour.apply(
-    definir_faixa_horaria)
+df_geral['faixa_horaria'] = df_geral['Tempo'].dt.hour.apply(definir_faixa_horaria)
 
 # Dicionário para mapear os valores de COB para os nomes das regiões
 cob_legend = {
@@ -87,41 +81,23 @@ cob_legend = {
     61: '6ºCOB - Varginha'
 }
 
-# Substituir os valores da coluna 'COB' pelos nomes das regiões
 df_geral['COB_nome'] = df_geral['COB'].map(cob_legend)
 
 # Função para converter os números dos meses em texto
-
-
 def convert_to_text(month):
     months_dict = {
-        0: 'Ano Todo',
-        1: 'Janeiro',
-        2: 'Fevereiro',
-        3: 'Março',
-        4: 'Abril',
-        5: 'Maio',
-        6: 'Junho',
-        7: 'Julho',
-        8: 'Agosto',
-        9: 'Setembro',
-        10: 'Outubro',
-        11: 'Novembro',
-        12: 'Dezembro'
+        0: 'Ano Todo', 1: 'Janeiro', 2: 'Fevereiro', 3: 'Março',
+        4: 'Abril', 5: 'Maio', 6: 'Junho', 7: 'Julho',
+        8: 'Agosto', 9: 'Setembro', 10: 'Outubro', 11: 'Novembro', 12: 'Dezembro'
     }
     return months_dict.get(month, "")
 
-
 # Gerar opções de meses com base no DataFrame
-options_month = [{'label': convert_to_text(
-    mes), 'value': mes} for mes in df_geral['Mês'].unique()]
+options_month = [{'label': convert_to_text(mes), 'value': mes} for mes in df_geral['Mês'].unique()]
 options_month = sorted(options_month, key=lambda x: x['value'])
-# Adicionar "Todos"
-# options_month.insert(0, {'label': 'Todos', 'value': 'all'})
 
 # Gerar opções de COBs com base no dicionário cob_legend
 options_cob = [{'label': cob_legend[key], 'value': key} for key in cob_legend]
-# options_cob.insert(0, {'label': 'Todos', 'value': 'all'})  # Adiciona "Todos"
 
 # Layout do Dashboard
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
@@ -129,142 +105,70 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 # =========  Layout  =========== #
 app.layout = dbc.Container(children=[
 
-    # Primeira Linha com Imagem, textos, toggle e filtros
     dbc.Row([
-        # Coluna 1: Imagem, textos e o botão toggle
         dbc.Col([
             html.Div([
-                html.Img(src='/assets/icon.png',  # Imagem da logo
-                         style={'width': '100px', 'height': '100px', 'object-fit': 'contain'})
+                html.Img(src='/assets/icon.png', style={'width': '100px', 'height': '100px', 'object-fit': 'contain'})
             ], style={'text-align': 'center'}),
-            html.Legend("Centrais Telefônicas", style={
-                'text-align': 'center', 'font-weight': 'bold', 'font-size': '1.5rem'}),
+            html.Legend("Centrais Telefônicas", style={'text-align': 'center', 'font-weight': 'bold', 'font-size': '1.5rem'}),
             html.Legend("CBMMG", style={'text-align': 'center'}),
-            ThemeSwitchAIO(aio_id="theme", themes=[url_theme1, url_theme2])
-        ], sm=12, md=4),  # Coluna 1 (Imagem e botões) ocupa 4/12 espaços
-
-        # Coluna 2: Filtro por mês
-        dbc.Col([
-            dbc.CardBody([
-                html.H3("Filtro por mês:", style={
-                    'text-align': 'center', 'font-weight': 'bold', 'font-size': '1rem', 'margin-bottom': '1rem', 'background-color': '#00244BFF', 'color': 'white', 'border': '1px solid black'}),
-                dcc.Checklist(
-                    id='filter-buttons',
-                    options=options_month,
-                    value=[option['value']
-                           for option in options_month if option['value'] != 'all'],
-                    inline=False,  # Permitir grid
-                )
-            ], style={'text-align': 'center', 'font-weight': 'bold', 'font-size': '.7rem', 'border': '1px solid black', 'height': '14rem'})
-        ], sm=12, md=4),  # Coluna 2 (Filtro por mês) ocupa 4/12 espaços
-
-        # Coluna 3: Filtro por COB
-        dbc.Col([
-            dbc.CardBody([
-                html.H3("Filtro por COB:", style={
-                    'text-align': 'center', 'font-weight': 'bold', 'font-size': '1rem', 'margin-bottom': '1rem', 'background-color': '#00244BFF', 'color': 'white', 'border': '1px solid black'}),
-                dcc.Checklist(
-                    id='cob-filter-buttons',
-                    options=options_cob,
-                    value=[key for key in cob_legend],
-                    inline=False,
-                )
-            ], style={'text-align': 'center', 'font-weight': 'bold', 'font-size': '.8rem', 'border': '1px solid black', 'height': '14rem'})
-        ], sm=12, md=4),  # Coluna 3 (Filtro por COB) ocupa 4/12 espaços
-    ], className='g-2 my-auto', style={'margin-top': '7px'}),
-
-    # Segunda Linha com Gráfico 1
-    dbc.Row([
-        dbc.Col([dbc.Card([
-            dbc.CardBody([
-                dcc.Graph(id='graph1', className='dbc', config=config_graph)
+            dbc.Row([
+            html.Div([
+                ThemeSwitchAIO(aio_id="theme", themes=[url_theme1, url_theme2]),
+                html.Div([
+              html.I(className="fas fa-eye", id="legend-eye", style={'margin-right': '10px'}),  # Ícone de olho
+                dbc.Switch(id='toggle-legend', value=True)
+                ], style={'display': 'flex', 'align-items': 'center'})
+  
+            ], style={'display': 'flex', 'align-items': 'center', 'margim': '100px', 'display': 'flex', 'flex-direction': 'row', 'justify-content': 'space-around'})  # Flexbox para alinhar ícone e switch
+        
             ])
-        ], style=tab_card)
-        ]),  # Ocupar toda a linha
+        ], sm=12, md=4),
+
+        dbc.Col([
+            dbc.CardBody([
+                html.H3("Filtro por mês:", style={'text-align': 'center', 'font-weight': 'bold', 'font-size': '1rem'}),
+                dcc.Checklist(id='filter-buttons', options=options_month, value=[mes['value'] for mes in options_month], inline=False)
+            ], style={'text-align': 'center', 'font-weight': 'bold', 'font-size': '.7rem', 'border': '1px solid black', 'height': '14rem'})
+        ], sm=12, md=4),
+
+        dbc.Col([
+            dbc.CardBody([
+                html.H3("Filtro por COB:", style={'text-align': 'center', 'font-weight': 'bold', 'font-size': '1rem'}),
+                dcc.Checklist(id='cob-filter-buttons', options=options_cob, value=[key for key in cob_legend], inline=False)
+            ], style={'text-align': 'center', 'font-weight': 'bold', 'font-size': '.8rem', 'border': '1px solid black', 'height': '14rem'})
+        ], sm=12, md=4),
     ], className='g-2 my-auto', style={'margin-top': '7px'}),
 
-    # Terceira Linha com Gráficos 2 e 3
-    dbc.Row([
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    dcc.Graph(id='graph2', className='dbc',
-                              config=config_graph)  # Gráfico 2
-                ])
-            ], style=tab_card)
-        ], sm=12, lg=6),
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    dcc.Graph(id='graph3', className='dbc',
-                              config=config_graph)  # Gráfico 3
-                ])
-            ], style=tab_card)
-        ], sm=12, lg=6),
-    ], className='g-2 my-auto', style={'margin-top': '7px'}),
+    dbc.Row([dbc.Col([dbc.Card([dbc.CardBody([dcc.Graph(id='graph1', className='dbc', config=config_graph)])], style=tab_card)], sm=12, lg=12)]),
 
-    # Quarta Linha com Gráfico 4
     dbc.Row([
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    dcc.Graph(id='graph4', className='dbc',
-                              config=config_graph)  # Gráfico 4 sozinho
-                ])
-            ], style=tab_card)
-        ], sm=12, lg=12),  # Ocupar toda a linha
-    ], className='g-2 my-auto', style={'margin-top': '7px'}),
+        dbc.Col([dbc.Card([dbc.CardBody([dcc.Graph(id='graph2', className='dbc', config=config_graph)])], style=tab_card)], sm=12, lg=6),
+        dbc.Col([dbc.Card([dbc.CardBody([dcc.Graph(id='graph3', className='dbc', config=config_graph)])], style=tab_card)], sm=12, lg=6),
+    ]),
 
-    # Quinta Linha com Gráficos 5 e 6
-    dbc.Row([
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    # Gráfico 5 ocupa 6 espaços
-                    dcc.Graph(id='graph5', className='dbc',
-                              config=config_graph)
-                ])
-            ], style=tab_card)
-        ], sm=12, lg=6),
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    # Gráfico 6 ocupa 6 espaços
-                    dcc.Graph(id='graph6', className='dbc',
-                              config=config_graph)
-                ])
-            ], style=tab_card)
-        ], sm=12, lg=6),
-    ], className='g-2 my-auto', style={'margin-top': '7px'}),
+    dbc.Row([dbc.Col([dbc.Card([dbc.CardBody([dcc.Graph(id='graph4', className='dbc', config=config_graph)])], style=tab_card)], sm=12, lg=12)]),
 
-    # Sexta Linha com Gráficos 7 e 8
     dbc.Row([
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    # Gráfico 7 ocupa 6 espaços
-                    dcc.Graph(id='graph7', className='dbc',
-                              config=config_graph)
-                ])
-            ], style=tab_card)
-        ], sm=12, lg=6),
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    # Gráfico 8 ocupa 6 espaços
-                    dcc.Graph(id='graph8', className='dbc',
-                              config=config_graph)
-                ])
-            ], style=tab_card)
-        ], sm=12, lg=6),
-    ], className='g-2 my-auto', style={'margin-top': '7px'}),
+        dbc.Col([dbc.Card([dbc.CardBody([dcc.Graph(id='graph5', className='dbc', config=config_graph)])], style=tab_card)], sm=12, lg=6),
+        dbc.Col([dbc.Card([dbc.CardBody([dcc.Graph(id='graph6', className='dbc', config=config_graph)])], style=tab_card)], sm=12, lg=6),
+    ]),
+
+    dbc.Row([
+        dbc.Col([dbc.Card([dbc.CardBody([dcc.Graph(id='graph7', className='dbc', config=config_graph)])], style=tab_card)], sm=12, lg=6),
+        dbc.Col([dbc.Card([dbc.CardBody([dcc.Graph(id='graph8', className='dbc', config=config_graph)])], style=tab_card)], sm=12, lg=6),
+    ]),
 
 ], fluid=True, style={'height': '100vh'})
 
-# Callback para Graficos
-# Callback para atualizar os filtros
+# Função para ajustar o layout dos gráficos (exibir/ocultar legendas)
+def adjust_graph_layout(fig, show_legend):
+    if show_legend:
+        fig.update_layout(showlegend=True)
+    else:
+        fig.update_layout(showlegend=False)
 
-
+# Callback para atualizar os gráficos com base nos filtros e no toggle de legenda
 @app.callback(
     [Output('graph1', 'figure'),
      Output('graph2', 'figure'),
@@ -276,12 +180,12 @@ app.layout = dbc.Container(children=[
      Output('graph8', 'figure')],
     [Input('filter-buttons', 'value'),
      Input('cob-filter-buttons', 'value'),
-     Input(ThemeSwitchAIO.ids.switch("theme"), "value")]
+     Input(ThemeSwitchAIO.ids.switch("theme"), "value"),
+     Input('toggle-legend', 'value')]
 )
-def update_graphs(month, cobs, toggle):
+def update_graphs(month, cobs, toggle, show_legend):
     template = template_theme1 if toggle else template_theme2
 
-    # Filtrar dados com base nos meses e COBs selecionados
     if 'all' not in month:
         df_filtered = df_geral[df_geral['Mês'].isin(month)]
     else:
@@ -290,56 +194,41 @@ def update_graphs(month, cobs, toggle):
     if 'all' not in cobs:
         df_filtered = df_filtered[df_filtered['COB'].isin(cobs)]
 
-    # Gráfico 1 - Quantidade de Ligações por Dia
-    chamadas_por_dia_cob = df_filtered.groupby(
-        [df_filtered['Tempo'].dt.date, 'COB_nome']).size().reset_index(name='Quantidade')
+    chamadas_por_dia_cob = df_filtered.groupby([df_filtered['Tempo'].dt.date, 'COB_nome']).size().reset_index(name='Quantidade')
     fig1 = px.bar(chamadas_por_dia_cob, x='Tempo', y='Quantidade', color='COB_nome',
                   labels={'Quantidade': 'Número de Chamadas', 'Tempo': 'Dia', 'COB_nome': 'Região (COB)'})
-    fig1.update_layout(legend_title_text='Região (COB)',
-                       legend=dict(traceorder='normal'), template=template, height=400)
+    fig1.update_layout(legend_title_text='Região (COB)', legend=dict(traceorder='normal'), template=template)
+    adjust_graph_layout(fig1, show_legend)
 
-    # Gráfico 2 - Ligações Atendidas e Não Atendidas por COB
-    atendidas_nao_atendidas = df_filtered.groupby(
-        ['COB_nome', 'Status']).size().reset_index(name='Quantidade')
+    atendidas_nao_atendidas = df_filtered.groupby(['COB_nome', 'Status']).size().reset_index(name='Quantidade')
     status_mapping = {0: 'Não Atendido', 1: 'Atendido'}
-    atendidas_nao_atendidas['Status'] = atendidas_nao_atendidas['Status'].map(
-        status_mapping)
+    atendidas_nao_atendidas['Status'] = atendidas_nao_atendidas['Status'].map(status_mapping)
     fig2 = px.bar(atendidas_nao_atendidas, x='COB_nome', y='Quantidade', color='Status',
                   labels={'Quantidade': 'Número de Chamadas', 'COB_nome': 'Região (COB)', 'Status': 'Atendimento'})
-    fig2.update_layout(legend_title_text='Atendimento',
-                       legend=dict(traceorder='normal'), template=template)
+    fig2.update_layout(legend_title_text='Atendimento', legend=dict(traceorder='normal'), template=template)
+    adjust_graph_layout(fig2, show_legend)
 
-    # Gráfico 3 - Ligações por Faixa Horária e COB
-    chamadas_por_faixa_horaria = df_filtered.groupby(
-        ['faixa_horaria', 'COB_nome']).size().reset_index(name='Quantidade')
-    faixa_horaria_ordem = ['00:00-02:00', '02:00-04:00', '04:00-06:00', '06:00-08:00', '08:00-10:00',
-                           '10:00-12:00', '12:00-14:00', '14:00-16:00', '16:00-18:00', '18:00-20:00',
-                           '20:00-22:00', '22:00-00:00']
-    chamadas_por_faixa_horaria['faixa_horaria'] = pd.Categorical(chamadas_por_faixa_horaria['faixa_horaria'],
-                                                                 categories=faixa_horaria_ordem, ordered=True)
+    chamadas_por_faixa_horaria = df_filtered.groupby(['faixa_horaria', 'COB_nome']).size().reset_index(name='Quantidade')
     fig3 = px.bar(chamadas_por_faixa_horaria, x='faixa_horaria', y='Quantidade', color='COB_nome',
                   labels={'Quantidade': 'Número de Chamadas', 'faixa_horaria': 'Faixa Horária', 'COB_nome': 'Região (COB)'})
-    fig3.update_layout(legend_title_text='Região (COB)',
-                       legend=dict(traceorder='normal'), template=template)
+    fig3.update_layout(legend_title_text='Região (COB)', legend=dict(traceorder='normal'), template=template)
+    adjust_graph_layout(fig3, show_legend)
 
-    # Gráfico 4 - Picos de Ligações por Faixa Horária e COB
-    chamadas_por_faixa_cob = df_filtered.groupby(
-        ['faixa_horaria', 'COB_nome']).size().reset_index(name='Quantidade')
+    chamadas_por_faixa_cob = df_filtered.groupby(['faixa_horaria', 'COB_nome']).size().reset_index(name='Quantidade')
     fig4 = px.line(chamadas_por_faixa_cob, x='faixa_horaria', y='Quantidade', color='COB_nome',
                    labels={'faixa_horaria': 'Faixa Horária', 'Quantidade': 'Número de Chamadas', 'COB_nome': 'Região (COB)'})
-    fig4.update_layout(legend_title_text='Região (COB)',
-                       legend=dict(traceorder='normal'), template=template)
+    fig4.update_layout(legend_title_text='Região (COB)', legend=dict(traceorder='normal'), template=template)
+    adjust_graph_layout(fig4, show_legend)
 
-    # Gráfico 5 - Distribuição de ligações atendidas por COB
     chamadas_atendidas = df_filtered[df_filtered['Status'] == 1]
-    atendidas_por_cob = chamadas_atendidas.groupby(
-        'COB_nome').size().reset_index(name='Quantidade')
+    atendidas_por_cob = chamadas_atendidas.groupby('COB_nome').size().reset_index(name='Quantidade')
     fig5 = px.pie(atendidas_por_cob, values='Quantidade', names='COB_nome',
                   title='Distribuição de ligações atendidas por COB (Região)',
                   labels={'COB_nome': 'Região (COB)', 'Quantidade': 'Número de Chamadas'})
     fig5.update_layout(template=template)
+    adjust_graph_layout(fig5, show_legend)
 
-    # Gráfico 6 - Top Atendente por número de ligações
+       # Gráfico 6 - Top Atendente por número de ligações
     chamadas_atendidas = df_filtered[(df_filtered['Status'] == 1) & (
         df_filtered['Teleatendente'] != '0')]
     atendidas_por_atendente = chamadas_atendidas.groupby(
@@ -427,7 +316,6 @@ def update_graphs(month, cobs, toggle):
     )
 
     return fig1, fig2, fig3, fig4, fig5, fig6, fig7, fig8
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
